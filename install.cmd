974f50c7b80a0ff263409738d20ca6f71479cb8c
test <test@yahoo.co.jp>
2021-01-17 22:02:48 +0900

    msbuildを使うように修正した

--- install.cmd
+++ install.cmd
@@ -1,32 +1,26 @@
 setlocal
 
-set IDE_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE
-set BUILD_PATH=C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+set IDE_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE
+set BUILD_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin
 set BUILD_TYPE=%1
 set CPU_TYPE=Any CPU
 set BATCH_FILE_FOLDER=%~dp0
 
 if "%1"=="" set BUILD_TYPE=Release
 
-"%IDE_PATH%\devenv" /build "%BUILD_TYPE%" ..\Windows\FooEditEngine.sln
-if errorlevel 1 goto end
-"%IDE_PATH%\devenv" /build "%BUILD_TYPE%" ..\WPF\FooEditEngine.sln
-if errorlevel 1 goto end
-"%IDE_PATH%\devenv" /build "%BUILD_TYPE%|%CPU_TYPE%" ..\UWP\FooEditEngine.UWP.sln
-if errorlevel 1 goto end
-"%BUILD_PATH%\MSBuild.exe" /p:Configuration=Release ..\Help\fooeditengine_api.shfbproj
-if errorlevel 1 echo "building help file is failed"
-
-pushd ..\Windows\FooEditEngine
-nuget pack FooEditEngine.csproj -Prop Configuration=%BUILD_TYPE% -Suffix %BUILD_TYPE% -Symbols -OutputDirectory "%BATCH_FILE_FOLDER%\dist"
+pushd ..\Windows\FooTextBox
+"%BUILD_PATH%\msbuild" -t:pack -p:Configuration=%BUILD_TYPE%"
+copy bin\%BUILD_TYPE%\*.nupkg "%BATCH_FILE_FOLDER%dist"
 popd
 
-pushd ..\WPF\FooEditEngine
-nuget pack FooEditEngine.csproj -Prop Configuration=%BUILD_TYPE% -Suffix %BUILD_TYPE% -Symbols -OutputDirectory "%BATCH_FILE_FOLDER%\dist"
+pushd ..\WPF\FooTextBox
+"%BUILD_PATH%\msbuild" -t:pack -p:Configuration=%BUILD_TYPE%"
+copy bin\%BUILD_TYPE%\*.nupkg "%BATCH_FILE_FOLDER%dist"
 popd
 
-pushd ..\UWP\FooEditEngine.UWP
-nuget pack FooEditEngine.UWP.csproj -Prop Configuration=%BUILD_TYPE% -Suffix %BUILD_TYPE% -Symbols -OutputDirectory "%BATCH_FILE_FOLDER%\dist"
+pushd ..\UWP\FooTextBox
+"%IDE_PATH%\devenv" /build "%BUILD_TYPE%|%CPU_TYPE%" ..\UWP.sln
+nuget pack FooEditEngine.UWP.nuspec -Prop Configuration=%BUILD_TYPE% -Suffix %BUILD_TYPE% -Symbols -OutputDirectory "%BATCH_FILE_FOLDER%\dist"
 popd
 
 :copy_dist
